f986697dd0ba17c90805d77d6508e19b5c2ecd34
soulsilver <122255949+soul0silver@users.noreply.github.com>
2023-11-23 09:58:49 +0700

    dat

---
+++ backend/course/src/main/java/com/course/model/Course.java
@@ -0,0 +1,31 @@
+package com.course.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "course")
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private int type;
+    private int uid;
+    private int cate;
+    private String description;
+    private Date createat;
+    private Date updateat;
+    private Date deleteat;
+    private boolean active;
+    private double price;
+
+}
