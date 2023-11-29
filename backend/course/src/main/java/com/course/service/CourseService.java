1b21915117b64b6f1e3292daaf2b68283740c8ea
soulsilver <122255949+soul0silver@users.noreply.github.com>
2023-11-29 16:45:41 +0700

    dat

--- backend/course/src/main/java/com/course/service/CourseService.java
+++ backend/course/src/main/java/com/course/service/CourseService.java
@@ -1,7 +1,10 @@
 package com.course.service;
 
+import com.course.DTO.VideoDto;
 import com.course.model.Course;
+import com.course.model.Document;
 import com.course.model.Route;
+import com.course.model.Video;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 
@@ -11,8 +14,11 @@ public interface CourseService {
     ResponseEntity<?> getListCourse(int page, String sort);
     ResponseEntity<?> save(Course course);
     ResponseEntity<?> findCourseById(Long id);
+    ResponseEntity<?> activeCourse(long id,boolean status);
     ResponseEntity<?> addRoutes(Route route);
     ResponseEntity<?> getAllRoute(long courseId);
     ResponseEntity<?> getVideo(long routeId);
+    ResponseEntity<?> addVideo(Video video);
     ResponseEntity<?> getDocument(long routeId);
+    ResponseEntity<?> addDoc(Document document);
 }
