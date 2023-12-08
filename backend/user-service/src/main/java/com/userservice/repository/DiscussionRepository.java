df8e2e36b9434d4a7ab806fba6caecdff038f8a9
NguyenHuyy0905 <huythuongtin@gmail.com>
2023-12-08 22:01:17 +0700

    Huy update

---
+++ backend/user-service/src/main/java/com/userservice/repository/DiscussionRepository.java
@@ -0,0 +1,10 @@
+package com.userservice.repository;
+
+import com.userservice.model.Discussion;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DiscussionRepository extends JpaRepository<Discussion, Long> {
+
+}
--- backend/user-service/src/main/java/com/userservice/service/DiscussionService.java
+++ backend/user-service/src/main/java/com/userservice/service/DiscussionService.java
@@ -5,5 +5,8 @@ import com.userservice.model.FeedBack;
 import org.springframework.http.ResponseEntity;
 
 public interface DiscussionService {
-
+    ResponseEntity<?> save(Discussion discussion);
+    ResponseEntity<?> deleteById(Long id);
+    Boolean existsById(Long id);
+    ResponseEntity<?> getAllOrderByTime(int page, int direction);
 }
