2a08d419f283992ce83da3b96f08846525efe6dc
soulsilver <122255949+soul0silver@users.noreply.github.com>
2023-12-09 01:12:05 +0700

    dat

--- backend/course/src/main/java/com/course/controller/CategoryController.java
+++ backend/course/src/main/java/com/course/controller/CategoryController.java
@@ -5,18 +5,19 @@ import com.course.service.imp.CategoryServiceImp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/sv2/category")
+@RequestMapping("api/sv2")
 public class CategoryController {
     @Autowired
     CategoryServiceImp service;
     @PostMapping("/private/category/add-cate")
     ResponseEntity<?> save(@RequestBody Category category){
         return service.save(category);
+    }
+    @GetMapping("/category/get-all")
+    ResponseEntity<?> getAll(int page,String sort){
+        return service.getAll(page,sort);
     }
 }
